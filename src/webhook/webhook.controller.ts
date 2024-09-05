7ca3adfbef58fe741fcd9ec108c74d0baf0463f5
Damian Taborda <dtaborda@securityscorecard.io>
2024-09-04 23:45:12 -0300

    feat(webhooks): updating webhooks code for mercado libre

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('webhook')
 export class WebhookController {
   @Post()
-  @HttpCode(HttpStatus.OK)
-  handleWebhook(@Body() body: any): string {
-    console.log('Webhook received:', body);
-    // Aquí puedes procesar el cuerpo del webhook según sea necesario
-    return 'Webhook processed successfully';
+  @HttpCode(HttpStatus.OK) // Asegura que siempre responda con HTTP 200
+  handleWebhook(@Body() body: any, @Res() res: Response) {
+    console.log('Webhook recibido:', body);
+
+    // Implementa aquí la lógica para manejar diferentes tipos de webhooks
+    if (body.type === 'subscription_preapproval') {
+      console.log('Manejando preaprobación de suscripción:', body.data);
+      res.status(HttpStatus.OK).send('Webhook procesado correctamente');
+    } else {
+      res.status(HttpStatus.BAD_REQUEST).send('Tipo de webhook no soportado');
+    }
   }
 }
