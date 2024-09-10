385008ef81b990e339da81e54b30fe4651c0546b
Damian Taborda <dtaborda@securityscorecard.io>
2024-09-09 22:23:17 -0300

    feat(payments): adding query param to search

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,6 +1,11 @@
 import { Controller, Get, Post, Put, Param, Body, Query } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
-import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiBearerAuth,
+  ApiQuery,
+} from '@nestjs/swagger';
 import { AuthToken } from '../auth/auth.decorator';
 import { UpdatePaymentDto } from '../dto/update-payment.dto';
 import { CreatePaymentDto } from '../dto/create-payment.dto';
@@ -38,6 +43,63 @@ export class PaymentsController {
 
   @Get('search')
   @ApiOperation({ summary: 'Search for payments' })
+  @ApiQuery({
+    name: 'sort',
+    required: false,
+    description: 'Sorting criteria',
+    example: 'date_created',
+  })
+  @ApiQuery({
+    name: 'criteria',
+    required: false,
+    description: 'Sort order',
+    example: 'desc',
+  })
+  @ApiQuery({
+    name: 'external_reference',
+    required: false,
+    description: 'External payment reference',
+  })
+  @ApiQuery({
+    name: 'status',
+    required: false,
+    description: 'Payment status',
+    example: 'approved',
+  })
+  @ApiQuery({
+    name: 'range',
+    required: false,
+    description: 'Range to search for',
+    example: 'date_created',
+  })
+  @ApiQuery({
+    name: 'begin_date',
+    required: false,
+    description: 'Start date for the search',
+    example: 'NOW-30DAYS',
+  })
+  @ApiQuery({
+    name: 'end_date',
+    required: false,
+    description: 'End date for the search',
+    example: 'NOW',
+  })
+  @ApiQuery({
+    name: 'store_id',
+    required: false,
+    description: 'Store ID for filtering',
+  })
+  @ApiQuery({
+    name: 'pos_id',
+    required: false,
+    description: 'POS ID for filtering',
+  })
+  @ApiQuery({
+    name: 'collector.id',
+    required: false,
+    description: 'Collector ID',
+  })
+  @ApiQuery({ name: 'payer.id', required: false, description: 'Payer ID' })
   async searchPayments(@Query() queryParams: any, @AuthToken() token: string) {
     return this.paymentsService.searchPayments(queryParams, token);
   }
